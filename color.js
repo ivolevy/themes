5567f11c42fb780352f490115dd39a88893bcb86
resultadosonlinecentralab <fifacuenta77@gmail.com>
2022-05-12 13:37:30 -0300

    pushing themes

---
+++ color.js
@@ -0,0 +1,6 @@
+function cambiar() {
+    document.getElementById('body').style.backgroundColor="#white";
+        }
+function reset(){
+    document.getElementById('body').style.backgroundColor="";
+        }
